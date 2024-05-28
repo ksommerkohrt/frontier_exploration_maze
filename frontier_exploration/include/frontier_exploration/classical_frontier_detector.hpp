194288db421c4d2c2342e0ab3a538a64df185571
ksommerkohrt <90289409+ksommerkohrt@users.noreply.github.com>
2024-05-28 16:22:48 -0400

    Update classical_frontier_detector.hpp

--- frontier_exploration/include/frontier_exploration/classical_frontier_detector.hpp
+++ frontier_exploration/include/frontier_exploration/classical_frontier_detector.hpp
@@ -28,7 +28,7 @@
 #include "tf2/exceptions.h"
 #include "tf2_ros/transform_listener.h"
 #include "tf2_ros/buffer.h"
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 
 using std::placeholders::_1;
 using std::placeholders::_2;
